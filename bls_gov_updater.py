e00a4a5b8cf50c9034384ed3506524446a39e887
Joseph Vorbeck <jvorbeck1@gmail.com>
2022-12-04 14:35:25 -0500

    clean up

--- bls_gov_updater.py
+++ bls_gov_updater.py
@@ -8,36 +8,43 @@ import requests as re
 import pandas as pd
 from datetime import datetime
 
-
-#sc = SparkContext.getOrCreate()
+sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
+
 #Current date for partitioning files
 today = datetime.today().strftime('%Y-%m-%d')
+
 #Specify bucket to write files to
 write_bucket = 'bls-timeseries-data'
+
 #Initialize boto3 client for s3 writes
 s3 = boto3.resource('s3')
+
 #List of datasets to loop through - in the case that these ever need to change in the future without editing reptitive code
 datasets_suffix = ["pr.data.0.Current", "pr.data.1.AllData", "pr.duration", "pr.footnote", "pr.measure", "pr.period", "pr.seasonal", "pr.sector", "pr.series"]
+
 #Loop through all items in the BLS directory, extract each dataset and store as an item in a list
 dataset_list = []
 for i in datasets_suffix:
     site_data_download = re.get("https://download.bls.gov/pub/time.series/pr/" + i).text
     dataset_list.append(site_data_download)    
+
 #For each dataset in the list, write to its respective folder in s3
 iterator = 0 
 for j in datasets_suffix:
     s3.Object(write_bucket, j + "/" + today + "/" + 'data.txt').put(Body=dataset_list[iterator])
     iterator += 1
+    
 #Initialize target bucket - will retreive most recent down
 target_bucket = s3.Bucket(write_bucket)
-## Bucket to use
+
+# Bucket to use
 bucket = s3.Bucket(write_bucket)
+
 most_recent = []
 previous = []
-
 #For each dataset obtain the most recent file to check against current day's run
 for i in datasets_suffix:
         all_obj_in_folder = list(s3.Bucket(write_bucket).objects.filter(Prefix=i))
@@ -48,7 +55,7 @@ for i in datasets_suffix:
             previous.append("s3://" + write_bucket + "/" + all_obj_in_folder[-2].key)
         except:
             print(i + " has no previous data")
-previous
+
 #Compare previously downloaded dataset to current day's run
 for i,j in zip(most_recent, previous):
     for x in datasets_suffix:
@@ -59,4 +66,5 @@ for i,j in zip(most_recent, previous):
             print("no changes to files")
         elif len(combine_dfs) != curr:
             combine_dfs.to_csv("s3://" + write_bucket + "/" + x + "/" + today + "/" + "data.csv")
+            
 job.commit()
